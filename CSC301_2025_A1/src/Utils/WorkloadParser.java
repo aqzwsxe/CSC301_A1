b03fce03db3023580ed5dd54f5fc024319ca1254
aqzwsxe <fzh200393@gmail.com>
2026-01-17 20:06:00 -0500

    1: Finished the ConfigReader class; 2: Create the WorkloadParser class

---
+++ CSC301_2025_A1/src/Utils/WorkloadParser.java
@@ -0,0 +1,13 @@
+public class WorkloadParser {
+
+    private static String orderUrl;
+
+    static void main(String[] args) {
+        if(args.length<1){
+            System.err.println("Missing the workload file");
+            System.exit(1);
+        }
+
+        orderUrl = ;
+    }
+}
